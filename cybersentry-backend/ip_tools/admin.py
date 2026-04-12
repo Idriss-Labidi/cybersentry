d435c41c6f20e247b91766395d4d4461b01b28f4
IdrisLaabidi <idrislaabidi@gmail.com>
2026-04-12 16:03:22 +0100

    refactor: remove unused admin classes for cleaner admin interface

--- cybersentry-backend/ip_tools/admin.py
+++ cybersentry-backend/ip_tools/admin.py
@@ -1,28 +1,3 @@
 from django.contrib import admin
 from .models import IPReputationScan
 
-
-@admin.register(IPReputationScan)
-class IPReputationScanAdmin(admin.ModelAdmin):
-    list_display = ['ip_address', 'user', 'reputation_score', 'risk_level', 'scanned_at']
-    list_filter = ['risk_level', 'is_proxy', 'is_hosting', 'is_mobile', 'scanned_at']
-    search_fields = ['ip_address', 'user__username']
-    readonly_fields = ['scanned_at']
-    ordering = ['-scanned_at']
-
-    fieldsets = (
-        ('Basic Information', {
-            'fields': ('user', 'ip_address', 'scanned_at')
-        }),
-        ('Reputation', {
-            'fields': ('reputation_score', 'risk_level', 'risk_factors')
-        }),
-        ('Flags', {
-            'fields': ('is_proxy', 'is_hosting', 'is_mobile')
-        }),
-        ('Location & Network', {
-            'fields': ('geolocation', 'network'),
-            'classes': ('collapse',)
-        }),
-    )
-
